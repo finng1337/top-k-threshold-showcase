37010b93ac764835d787e77f365b454848b1648f
Radek Čermák <cermara7@cvut.cz>
2023-04-13 14:34:35 +0200

    optimize naive algorithm for top-k

--- backend/processor/views.py
+++ backend/processor/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .algorithms import get_k_objects
+from .serializers import ProcessorSerializer
 import time
 
 class ProcessorListView(APIView):
@@ -9,13 +10,15 @@ class ProcessorListView(APIView):
         fields = self.request.query_params.getlist('fields')
         algorithm = self.request.query_params.get('algorithm')
         aggrFunc = self.request.query_params.get('aggr_func')
+        k = self.request.query_params.get('k')
 
         start = time.time()
-        rowsRead, serializedProcs = get_k_objects(2, fields, algorithm, aggrFunc)
+        rowsRead, processors = get_k_objects(int(k), fields, algorithm, aggrFunc)
         elapsedTime = time.time() - start
+        serializedProcs = ProcessorSerializer(processors, many=True)
 
         return Response({
             'time': elapsedTime,
             'rows_read': rowsRead,
-            'data': serializedProcs
+            'data': serializedProcs.data
         }, status=status.HTTP_200_OK)
