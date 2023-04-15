20c9b412bfbd27abab94c9f23b1c60a9a697bc1f
Radek Čermák <cermara7@cvut.cz>
2023-04-15 19:21:47 +0200

    change types of normalized fields

--- backend/processor/models.py
+++ backend/processor/models.py
@@ -3,19 +3,22 @@ from django.db import models
 class Processor(models.Model):
     name = models.CharField(max_length=64)
     cores = models.IntegerField()
-    cores_normalized = models.DecimalField(max_digits=5, decimal_places=4)
+    cores_normalized = models.FloatField()
     threads = models.IntegerField()
-    threads_normalized = models.DecimalField(max_digits=6, decimal_places=5)
+    threads_normalized = models.FloatField()
     frequency = models.IntegerField()
-    frequency_normalized = models.DecimalField(max_digits=5, decimal_places=4)
+    frequency_normalized = models.FloatField()
     boost_frequency = models.IntegerField()
-    boost_frequency_normalized = models.DecimalField(max_digits=5, decimal_places=4)
+    boost_frequency_normalized = models.FloatField()
     cache = models.IntegerField()
-    cache_normalized = models.DecimalField(max_digits=8, decimal_places=7)
+    cache_normalized = models.FloatField()
     lithography = models.IntegerField()
-    lithography_normalized = models.DecimalField(max_digits=5, decimal_places=4)
+    lithography_normalized = models.FloatField()
     tdp = models.IntegerField()
-    tdp_normalized = models.DecimalField(max_digits=6, decimal_places=5)
+    tdp_normalized = models.FloatField()
 
     def __str__(self):
         return self.name
+
+    def __lt__(self, other):
+        return self.name < other.name
--- backend/scripts/load_data.py
+++ backend/scripts/load_data.py
@@ -1,5 +1,4 @@
 from processor.models import Processor
-from decimal import Decimal
 import csv
 
 def run():
@@ -13,18 +12,18 @@ def run():
             processor = Processor(
                 name=row[0],
                 cores=row[1],
-                cores_normalized=Decimal(row[2].replace(',', '.')),
+                cores_normalized=row[2].replace(',', '.'),
                 threads=row[3],
-                threads_normalized=Decimal(row[4].replace(',', '.')),
+                threads_normalized=row[4].replace(',', '.'),
                 frequency=row[5],
-                frequency_normalized=Decimal(row[6].replace(',', '.')),
+                frequency_normalized=row[6].replace(',', '.'),
                 boost_frequency=row[7],
-                boost_frequency_normalized=Decimal(row[8].replace(',', '.')),
+                boost_frequency_normalized=row[8].replace(',', '.'),
                 cache=row[9],
-                cache_normalized=Decimal(row[10].replace(',', '.')),
+                cache_normalized=row[10].replace(',', '.'),
                 lithography=row[11],
-                lithography_normalized=Decimal(row[12].replace(',', '.')),
+                lithography_normalized=row[12].replace(',', '.'),
                 tdp=row[13],
-                tdp_normalized=Decimal(row[14].replace(',', '.')),
+                tdp_normalized=row[14].replace(',', '.'),
             )
             processor.save()
