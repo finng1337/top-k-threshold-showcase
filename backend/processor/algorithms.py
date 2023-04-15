6c1a3ddbf6eb47fddc71b17a686b47637b94c8b0
Radek Čermák <cermara7@cvut.cz>
2023-04-15 19:26:24 +0200

    add treshold algorithm of top-k

--- backend/processor/algorithms.py
+++ backend/processor/algorithms.py
@@ -1,4 +1,5 @@
 import numpy as np
+import heapq
 from .models import Processor
 
 
@@ -6,24 +7,80 @@ def get_k_objects(k: int, fields: list[str], algorithm: str, aggr_func: str) -> 
     match algorithm:
         case 'naive':
             return get_k_naive(k, fields, get_aggr_func(aggr_func))
+        case 'treshold':
+            return get_k_treshold(k, fields, get_aggr_func(aggr_func))
 
-def get_k_naive(k: int, fields: list[str], aggr_func) -> tuple[int,list]:
+def get_k_treshold(k: int, fields: list[str], aggr_func: callable) -> tuple[int,list]:
+    fieldsList = []
+    fieldsIndexes = {}
+    for field in fields:
+        fieldName, order = field.split('_', 1)
+        fieldsList.append((fieldName + '_normalized', order))
+        fieldsIndexes[fieldName + '_normalized'] = get_field_index(fieldName, order)
+    seenIds = set()
+    processorRank = []
+    readRows = 0
+    heapq.heapify(processorRank)
+
+    for i in range(len(fieldsIndexes[fieldsList[0][0]])):
+        readRows += 1
+        tresholdValues = []
+        for field in fieldsList:
+            processor = fieldsIndexes[field[0]][i]
+            tresholdValues.append(getattr(processor, field[0]) if field[1] == 'desc' else abs(1 - getattr(processor, field[0])))
+            if processor.id not in seenIds:
+                seenIds.add(processor.id)
+                heapq.heappush(processorRank, (get_proc_rank(processor, fieldsList, aggr_func), processor))
+                if len(processorRank) > k:
+                    heapq.heappop(processorRank)
+        treshold = aggr_func(tresholdValues)
+        if processorRank[0][0] >= treshold and len(processorRank) >= k:
+            return readRows, np.array(heapq.nlargest(k, processorRank))[:, 1]
+    return 0, []
+
+def get_k_naive(k: int, fields: list[str], aggr_func: callable) -> tuple[int,list]:
     processors = list(Processor.objects.all())
-    processors_rank = []
+    processorsRank = []
+    fieldsList = []
+
+    for field in fields:
+        fieldName, order = field.split('_', 1)
+        fieldsList.append((fieldName + '_normalized', order))
 
     for processor in processors:
-        fieldValues = []
-        for field in fields:
-            fieldName, order = field.split('_')
-            fieldName += '_normalized'
-            fieldValue = getattr(processor, fieldName) if order == 'desc' else abs(1 - getattr(processor, fieldName))
-            fieldValues.append(fieldValue)
-        processors_rank.append([processor, aggr_func(fieldValues)])
-    processors_rank.sort(key=lambda pair: pair[1], reverse=True)
+        processorsRank.append((get_proc_rank(processor, fieldsList, aggr_func), processor))
+    processorsRank.sort(reverse=True)
 
-    return len(processors), list(np.array(processors_rank[:k])[:, 0])
+    return len(processors), list(np.array(processorsRank[:k])[:, 1])
 
-def get_aggr_func(aggr_func: str):
+def get_proc_rank(processor: Processor, fields: list[tuple[str, str]], aggr_func: callable):
+    fieldValues = []
+    for field in fields:
+        fieldValue = getattr(processor, field[0]) if field[1] == 'desc' else abs(1 - getattr(processor, field[0]))
+        fieldValues.append(fieldValue)
+    return aggr_func(fieldValues)
+
+def get_field_index(field: str, order: str) -> list:
+    fieldName = field + '_normalized'
+    orderBy = fieldName if order == 'asc' else '-' + fieldName
+
+    match field:
+        case 'cores':
+            return list(Processor.objects.all().order_by(orderBy))
+        case 'threads':
+            return list(Processor.objects.all().order_by(orderBy))
+        case 'frequency':
+            return list(Processor.objects.all().order_by(orderBy))
+        case 'boost_frequency':
+            return list(Processor.objects.all().order_by(orderBy))
+        case 'cache':
+            return list(Processor.objects.all().order_by(orderBy))
+        case 'lithography':
+            return list(Processor.objects.all().order_by(orderBy))
+        case 'tdp':
+            return list(Processor.objects.all().order_by(orderBy))
+
+def get_aggr_func(aggr_func: str) -> callable:
     match aggr_func:
         case 'sum':
             return sum
