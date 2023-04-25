3acf85b6ec5d15b08314ddbbde0c4987c097176d
Radek Čermák <cermara7@cvut.cz>
2023-04-25 10:21:40 +0200

    Add generated processors

--- backend/processor/models.py
+++ backend/processor/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
 class Processor(models.Model):
-    name = models.CharField(max_length=64)
-    cores = models.IntegerField()
+    type = models.CharField(max_length=64)
+    name = models.CharField(max_length=128, default='')
+    cores = models.IntegerField(blank=True, null=True)
     cores_normalized = models.FloatField()
-    threads = models.IntegerField()
+    threads = models.IntegerField(blank=True, null=True)
     threads_normalized = models.FloatField()
-    frequency = models.IntegerField()
+    frequency = models.IntegerField(blank=True, null=True)
     frequency_normalized = models.FloatField()
-    boost_frequency = models.IntegerField()
+    boost_frequency = models.IntegerField(blank=True, null=True)
     boost_frequency_normalized = models.FloatField()
-    cache = models.IntegerField()
+    cache = models.IntegerField(blank=True, null=True)
     cache_normalized = models.FloatField()
-    lithography = models.IntegerField()
+    lithography = models.IntegerField(blank=True, null=True)
     lithography_normalized = models.FloatField()
-    tdp = models.IntegerField()
+    tdp = models.IntegerField(blank=True, null=True)
     tdp_normalized = models.FloatField()
 
     def __str__(self):
--- backend/scripts/load_data.py
+++ backend/scripts/load_data.py
@@ -2,28 +2,51 @@ from processor.models import Processor
 import csv
 
 def run():
+    Processor.objects.all().delete()
+
     with open('datasets/amd.csv') as file:
         reader = csv.reader(file)
         next(reader)
 
-        Processor.objects.all().delete()
-
         for row in reader:
             processor = Processor(
+                type='real',
                 name=row[0],
                 cores=row[1],
-                cores_normalized=row[2].replace(',', '.'),
+                cores_normalized=row[2],
                 threads=row[3],
-                threads_normalized=row[4].replace(',', '.'),
+                threads_normalized=row[4],
                 frequency=row[5],
-                frequency_normalized=row[6].replace(',', '.'),
+                frequency_normalized=row[6],
                 boost_frequency=row[7],
-                boost_frequency_normalized=row[8].replace(',', '.'),
+                boost_frequency_normalized=row[8],
                 cache=row[9],
-                cache_normalized=row[10].replace(',', '.'),
+                cache_normalized=row[10],
                 lithography=row[11],
-                lithography_normalized=row[12].replace(',', '.'),
+                lithography_normalized=row[12],
                 tdp=row[13],
-                tdp_normalized=row[14].replace(',', '.'),
+                tdp_normalized=row[14],
             )
             processor.save()
+    print('Real processors imported')
+    print('Importing generated processors')
+    with open('datasets/experiment.csv') as file:
+        i = 0
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            if(i % 1000 == 0):
+                print('Imported: ' + i)
+            i += 1
+            processor = Processor(
+                type='experiment',
+                cores_normalized=row[0],
+                threads_normalized=row[1],
+                frequency_normalized=row[2],
+                boost_frequency_normalized=row[3],
+                cache_normalized=row[4],
+                lithography_normalized=row[5],
+                tdp_normalized=row[6],
+            )
+            processor.save()
+    print('Generated processors imported')
