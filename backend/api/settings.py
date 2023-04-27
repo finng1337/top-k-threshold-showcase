fa9ca2ca9836b9446983147c3e3da7942db98bd7
Radek Čermák <cermara7@cvut.cz>
2023-04-27 20:45:57 +0200

    Add frontend to docker

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -19,9 +19,11 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'processor',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +49,11 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://localhost:3000",
 ]
 
 WSGI_APPLICATION = 'api.wsgi.application'
